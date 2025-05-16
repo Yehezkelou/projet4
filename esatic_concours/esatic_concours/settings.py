47c6dcde316b9093ffbfa743ba1b297fa8cf9dee
Yehezkelou <TokapieuOEz@gmail.com>
2025-05-16 10:19:22 +0000

    Update settings.py

--- esatic_concours/esatic_concours/settings.py
+++ esatic_concours/esatic_concours/settings.py
@@ -29,9 +29,9 @@ SECRET_KEY = 'django-insecure-vzu58p+#j$r3@)h3s%gg(((p6_l2zxy*76u_bydw=njxm9=3tb
 DEBUG = True
 
 if DEBUG:
-    ALLOWED_HOSTS = ['127.0.0.1']
+    ALLOWED_HOSTS = ['16.171.169.237']
 else:
-    ALLOWED_HOSTS = ['56.228.26.247', 'esatic-concours.assemiendev.me']
+    ALLOWED_HOSTS = ['16.171.169.237']
 
 
 
